937ceff389bc7a27fac77a95cc5f3f13692a37f5
asaio <59561420+asaio@users.noreply.github.com>
2023-09-27 04:58:11 -0300

    Update versioning_controller.py

--- versioning_controller.py
+++ versioning_controller.py
@@ -1,7 +1,7 @@
 class VersioningController:
-    def __init__(self, s3_adapter, ssm_adapter, versioning_history):
+    def __init__(self, s3_adapter, ssm_adapter):
         # Instantiate the UpdateVersioningUseCase with the dependencies
-        self.update_versioning_use_case = UpdateVersioningUseCase(s3_adapter, ssm_adapter, versioning_history)
+        self.update_versioning_use_case = , ssm_adapter)
 
     def validate_event(self, event):
         # Check if the event contains "/tag/name"
@@ -9,7 +9,7 @@ class VersioningController:
 
     def process_event(self, bucket_name):
         # Check if an update is required and get the updated versioning configuration
-        update_required, updated_versioning = self.update_versioning_use_case.assure_versioning_configuration(bucket_name)
+        update_required, updated_versioning = UpdateVersioningUseCase(self.s3_adapter, self.ssm_adapter).assure_versioning_configuration(bucket_name)
 
         if update_required:
             # Log or perform any necessary actions based on the update
